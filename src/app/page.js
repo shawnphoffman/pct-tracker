7dc01e2b9ab4ebd159faa158d31a9f992564eec6
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-09-23 17:06:11 -0700

    Basic layer toggles

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 'use client'
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState, useCallback } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
+const Layers = {
+	Madi2023: 'PCT - 2023',
+	Madi2019: 'PCT - 2019',
+	Madi2018: 'PCT - 2018',
+}
 
 const Home = () => {
 	const mapContainer = useRef(null)
@@ -11,6 +17,27 @@ const Home = () => {
 	const [lng, setLng] = useState(-121.577)
 	const [lat, setLat] = useState(41.369)
 	const [zoom, setZoom] = useState(4.75)
+
+	const toggleLayer = useCallback(
+		layerName => {
+			if (!map.current) {
+				console.log('NO MAP')
+				return
+			}
+
+			const visibility = map.current.getLayoutProperty(layerName, 'visibility')
+
+			console.log('Toggling layer', {
+				layerName,
+				currentVisibility: visibility,
+				newVisibility: visibility === 'none' ? 'visible' : 'none',
+			})
+
+			// Toggle layer visibility by changing the layout object's visibility property.
+			map.current.setLayoutProperty(layerName, 'visibility', visibility === 'none' ? 'visible' : 'none')
+		},
+		[map]
+	)
 
 	useEffect(() => {
 		if (map.current) return // initialize map only once
@@ -20,10 +47,16 @@ const Home = () => {
 			center: [lng, lat],
 			zoom: zoom,
 		})
+		window.map = map.current
 	})
 
 	return (
 		<div>
+			<nav>
+				<button onClick={() => toggleLayer('PCT - 2023')}>2023</button>
+				<button onClick={() => toggleLayer('PCT - 2019')}>2019</button>
+				<button onClick={() => toggleLayer('PCT - 2018')}>2018</button>
+			</nav>
 			<div ref={mapContainer} className="map-container" />
 		</div>
 	)
