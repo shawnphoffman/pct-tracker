8062aacd92c8ad0cd04b98ae1e307d036c651dc7
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-12-27 13:19:29 -0500

    Stop double=loading images

--- src/components/PhotoLightbox.js
+++ src/components/PhotoLightbox.js
@@ -1,9 +1,14 @@
 import Lightbox from 'yet-another-react-lightbox'
-import 'yet-another-react-lightbox/styles.css'
+import Counter from 'yet-another-react-lightbox/plugins/counter'
 import NextJsImage from '@/components/NextJsImage'
+import 'yet-another-react-lightbox/styles.css'
+import 'yet-another-react-lightbox/plugins/counter.css'
 
-const images = require.context('@/images/', true, /\.(jpg|jpeg|png)$/i)
+// const images = require.context('../images/', true, /\.(jpg|jpeg|png)$/i)
+const images = require.context('../images/', true, /\.\/.*\.(jpg|jpeg|png)$/i)
 const imageList = images.keys().map(image => images(image).default)
+
+// console.log('length', JSON.stringify(images.keys().sort(), null, 2))
 
 export default function PhotoLightbox({ isOpen, setIsOpen, imageOverride }) {
 	let index
@@ -12,5 +17,14 @@ export default function PhotoLightbox({ isOpen, setIsOpen, imageOverride }) {
 		index = imageList.findIndex(image => image.src.includes(imageOverride.replace(/\.jpg|\.jpeg|\.png/, '')))
 	}
 
-	return <Lightbox open={isOpen} index={index} close={() => setIsOpen(false)} slides={imageList} render={{ slide: NextJsImage }} />
+	return (
+		<Lightbox
+			open={isOpen}
+			index={index}
+			close={() => setIsOpen(false)}
+			slides={imageList}
+			render={{ slide: NextJsImage }}
+			plugins={[Counter]}
+		/>
+	)
 }
