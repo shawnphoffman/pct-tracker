0b8f3fb0ebddf66cbe7b4e53fc14cb004b06948a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-24 15:22:04 -0700

    Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
 	title: 'PCT Tracker',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body>{children}</body>
+			<Analytics />
 		</html>
 	)
 }
